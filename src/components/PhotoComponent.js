00f9e3a473ad07a405bd23a5c1708016db47e426
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-03-12 20:11:01 +0500

    adding PhotoComponent and NotFound modules

---
+++ src/components/PhotoComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoComponent = () => {
+    return (
+      <li>
+        <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+      </li>
+    );
+}
+
+export default PhotoComponent;
