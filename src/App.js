5afc0509a57bc9a787a68741790d9f99433dad68
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-03-13 23:16:55 +0500

    Setting API key variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import apiKey from './config';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
+
+//console.log(apiKey);
 
 
 function App() {
