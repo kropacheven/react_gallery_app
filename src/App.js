e1a5d7d057b0427c5618caae7ba65354c135287a
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-03-14 00:05:58 +0500

    adding axios to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import apiKey from './config';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
@@ -7,6 +8,19 @@ import PhotoContainer from './components/PhotoContainer';
 //console.log(apiKey);
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  useEffect(() => {
+   axios.get(apiKey)
+    .then(response => {
+      //handle success
+      console.log(response);
+    })
+    .catch(error => {
+      //handle error
+      console.log("Error while fetching and parsing data", error);
+    }) 
+  }, []);
+
   return (
     <div>
       <SearchForm />
